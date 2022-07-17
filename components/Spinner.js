c86620faadbbda0fbda9847f519061db53a0a0eb
Umair Mirza <umair_hp@hotmail.com>
2022-07-17 22:56:16 +0500

    ran the build script

--- components/Spinner.js
+++ components/Spinner.js
@@ -1,8 +1,10 @@
 
+import Image from 'next/image'
+
 export default function Spinner() {
     return (
         <div>
-            <img className="h-20 w-20" src="/images/spinner.gif" />
+            <Image width="100" height="100" src="/images/spinner.gif" alt="Spinner" />
         </div>
     )
 }
